29ad822e27d46b14fd990d9d55dd231904b44a20
Ubuntu <tester@techpearl.com>
2022-10-08 02:36:11 +0530

    change user schema with firstname, lastname and email

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
+  firstName: String,
+  lastName: String,
+  email: String,
   passwordHash: String,
   sketches: [
     {
