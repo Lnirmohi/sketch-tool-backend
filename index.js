d21924e17d762a2890bf40ed391166ef638f39e2
Ubuntu <tester@techpearl.com>
2022-10-08 02:33:45 +0530

    add morgan middleware for logging

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const Sketch = require('./models/sketch');
 const usersRouter = require('./controllers/users');
 const loginRuter = require('./controllers/login');
 const sketchRouter = require("./controllers/sketch");
+const morgan = require("morgan")
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRuter);
